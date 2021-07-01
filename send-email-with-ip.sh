49faa815227158fdffd8d700e8aac6f6bcdd748b
esdandreu <esdandreu@gmail.com>
2021-07-01 18:41:36 +0200

    testing

--- send-email-with-ip.sh
+++ send-email-with-ip.sh
@@ -1,7 +1,7 @@
 source .env
-echo $EMAIL
+echo "$EMAIL"
 $PUBLICIP=`wget -qO- https://ipecho.net/plain`
-echo $PUBLICIP
+echo "$PUBLICIP"
 sendemail -l email.log     \
     -f $EMAIL \
     -u "What is my IP" \
